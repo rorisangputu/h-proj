21fe903ce28875bbea915239d0ec0c207ac41ca1
Rorisang Putu <rorisangputu@gmail.com>
2024-12-15 01:05:27 +0200

    adding cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,25 @@
 
 import express, { Request, Response, urlencoded } from 'express'; 
 import cors from 'cors';
-import 'dotnev/config';
+import 'dotenv/config';
 
 
 const app = express();
 app.use(express.json());
 app.use(urlencoded({
     extended: true
-}))
+}));
+app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+
 
 app.get("/", (req: Request, res: Response) => {
     return res.send("BZZZAAA world");
    
 });
 
-app.listen(3000, () => {
-    console.log("Listening on 3k");
+app.listen(PORT, () => {
+    console.log("Listening on port:", PORT);
     
 });
