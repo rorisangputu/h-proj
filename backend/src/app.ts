e8b23dc719b97ce187efc7f8f323533276a6f215
Rorisang Putu <rorisangputu@gmail.com>
2024-12-15 00:54:12 +0200

    configuring express

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
 
-import express, { Request, Response } from 'express'; 
+import express, { Request, Response, urlencoded } from 'express'; 
 import cors from 'cors';
 import 'dotnev/config';
 
 
 const app = express();
+app.use(express.json());
+app.use(urlencoded({
+    extended: true
+}))
 
 app.get("/", (req: Request, res: Response) => {
     return res.send("BZZZAAA world");
