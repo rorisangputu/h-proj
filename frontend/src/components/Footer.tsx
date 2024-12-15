af5d7b3924d734d5b1d381a56636364955fb06bc
Rorisang Putu <rorisangputu@gmail.com>
2024-12-16 00:19:01 +0200

    Creating footer component

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div className="bg-blue-800 py-10">
+      <div className="w-[60%] mx-auto flex justify-between items-center">
+        <span className="text-3xl text-white font-semibold tracking-tight">
+          MernHolidays.com
+        </span>
+        <span className="text-white font-bold tracking-tight flex gap-4">
+                  
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
