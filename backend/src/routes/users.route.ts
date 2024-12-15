3f1ed8893072cd70779975865ecd17b6497f3543
Rorisang Putu <rorisangputu@gmail.com>
2024-12-16 01:22:42 +0200

    importing user model

--- backend/src/routes/users.route.ts
+++ backend/src/routes/users.route.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from 'express';
+import User from '../models/user.model';
 
 const router = express.Router();
 
 router.post("/register", async (req: Request, res: Response) => {
     try {
-        
+        let user = await User.findOne({
+            email: req.body.email,
+        })
     } catch (error) {
         
     }
