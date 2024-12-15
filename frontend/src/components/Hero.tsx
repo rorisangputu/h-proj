31146e40a8fcba742a04b5c1bd4adf29440499c6
Rorisang Putu <rorisangputu@gmail.com>
2024-12-16 00:04:33 +0200

    fixing component width

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,8 +1,10 @@
 const Hero = () => {
   return (
     <div className="bg-blue-800 pb-16">
-      <div className="container mx-auto flex flex-col gap-2">
-        <h1 className="text-5xl text-white fon-bold">Find Your Next Stay</h1>
+      <div className="w-[60%] mx-auto flex flex-col gap-2">
+        <h1 className="text-5xl text-white font-semibold">
+          Find Your Next Stay
+        </h1>
         <p className="text-2xl text-white">
           Search low prices on hotel for your dream location...
         </p>
