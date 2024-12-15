a11117935abd9d873f870c417aa993035f16aca9
Rorisang Putu <rorisangputu@gmail.com>
2024-12-16 01:20:40 +0200

    creating reigster route

---
+++ backend/src/routes/users.route.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+
+const router = express.Router();
+
+router.post("/register", async (req: Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
