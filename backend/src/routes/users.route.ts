ad5ce9e63a0d7502046b1672309bd7fb8619418c
Rorisang Putu <rorisangputu@gmail.com>
2024-12-16 01:27:58 +0200

    handling user creation and errors

--- backend/src/routes/users.route.ts
+++ backend/src/routes/users.route.ts
@@ -7,8 +7,16 @@ router.post("/register", async (req: Request, res: Response) => {
     try {
         let user = await User.findOne({
             email: req.body.email,
-        })
+        });
+
+        if (user) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        user = new User(req.body);
+        await user.save()
     } catch (error) {
-        
+        console.log(error);
+        res.status(500).send({ message: "Something went wrong" });
     }
 })
