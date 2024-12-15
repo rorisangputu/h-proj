cbfeb082fed361d7e1b2627d5ca56ce4ae2e1a35
Rorisang Putu <rorisangputu@gmail.com>
2024-12-16 01:36:43 +0200

    adding logic to encrypt password before save

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcryptjs';
 
 
 export type UserType = {
@@ -32,7 +33,10 @@ const userSchema = new Schema({
 });
 
 userSchema.pre("save", async function(next) {
-    
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8)
+    }
+    next();
 })
 const User = mongoose.model<UserType>("User", userSchema);
 
