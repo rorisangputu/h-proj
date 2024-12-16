8d1aa1e93fe4f9359f48d575299fd7ecdb509148
Rorisang Putu <rorisangputu@gmail.com>
2024-12-16 16:16:56 +0200

    adding and styling register page title

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,11 @@
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <form className="flex flex-col gap-5">
+        <h2 className="text-3xl font-bold">Create an account</h2>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
