6e3f31da2f31023c3a860e61172cd083077cb63b
Rorisang Putu <rorisangputu@gmail.com>
2024-12-16 16:26:04 +0200

    creating form data types

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,7 +1,15 @@
 import { useForm } from "react-hook-form";
 
+type RegisterFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 const Register = () => {
-  const { register } = useForm();
+  const { register } = useForm<RegisterFormData>();
   return (
     <div>
       <form className="flex flex-col gap-5">
