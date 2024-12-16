8ad487c0515cd888c817338348b9ccb34bc232e3
Rorisang Putu <rorisangputu@gmail.com>
2024-12-16 12:26:16 +0200

    commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 import userRoutes from './routes/users.route';
 
+//Database Connection
 const dbConn = async () => {
     try {
         await mongoose.connect(process.env.MONGO as string)
@@ -14,7 +15,7 @@ const dbConn = async () => {
     }
 };
 
-
+//Initialising app
 const app = express();
 app.use(express.json());
 app.use(urlencoded({
@@ -23,10 +24,12 @@ app.use(urlencoded({
 app.use(cors());
 const PORT = process.env.PORT || 3000;
 
-
-
+//Creating api routes
 app.use("/api/users", userRoutes);
 
+
+
+//App listener
 app.listen(PORT, () => {
     dbConn();
     console.log("Listening on port:", PORT);
