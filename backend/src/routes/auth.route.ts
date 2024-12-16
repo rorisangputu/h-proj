bb6a5d8c83016e07e2b2671c14d36c2214c20b37
Rorisang Putu <rorisangputu@gmail.com>
2024-12-16 13:32:33 +0200

    setting login route and validator middleware

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response } from 'express';
+import { check } from 'express-validator';
 
 const router = express.Router();
 
-
+router.post("/login", [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more character is required").isLength({
+        min: 6
+    })
+], async (req: Request, res: Response) => {
+    
+})
 
 export default router;
