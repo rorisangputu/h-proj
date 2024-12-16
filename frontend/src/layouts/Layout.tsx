7856b9eeb89bbf96a7416db0ecfca4ad74807a1f
Rorisang Putu <rorisangputu@gmail.com>
2024-12-16 16:43:18 +0200

    changing children width

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -11,7 +11,7 @@ const Layout = ({ children }: LayoutProps) => {
     <div className="flex flex-col min-h-screen">
       <Header />
       <Hero />
-      <div className="w-[60%] mx-auto py-10 flex-1">{children}</div>
+      <div className="w-[90%] md:w-[60%] mx-auto py-10 flex-1">{children}</div>
       <Footer />
     </div>
   );
