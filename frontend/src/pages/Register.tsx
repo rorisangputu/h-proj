eac1c0f967def3d0856ccf40411f4e21bcdd1082
Rorisang Putu <rorisangputu@gmail.com>
2024-12-16 16:39:24 +0200

    styling inputs

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -15,22 +15,22 @@ const Register = () => {
       <form className="flex flex-col gap-5">
         <h2 className="text-3xl font-bold">Create an account</h2>
         <div className="flex flex-col md:flex-row gap-5">
-          <label htmlFor="" className="text-gray-700 text-sm font-bold">
+          <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
             First Name
             <input
               type="text"
               name=""
               id=""
-              className="border rounded w-full py-1 px-2 font-normal"
+              className="border rounded w-full py-2 px-2 mt-3 font-normal"
             />
           </label>
-          <label htmlFor="" className="text-gray-700 text-sm font-bold">
+          <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
             Last Name
             <input
               type="text"
               name=""
               id=""
-              className="border rounded w-full py-1 px-2 font-normal"
+              className="border rounded w-full py-2 px-2 mt-3 font-normal"
             />
           </label>
         </div>
