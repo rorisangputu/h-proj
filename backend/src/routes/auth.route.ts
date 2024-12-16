76faa02b837821506ae48be75540fd02e9bccde7
Rorisang Putu <rorisangputu@gmail.com>
2024-12-16 13:49:15 +0200

    returning a message if passwords dont match

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -24,6 +24,7 @@ router.post("/login", [
             if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
             const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) return res.status(400).json({ message: "Invalid Credentials" });
             
         } catch (error) {
             console.log(error);
