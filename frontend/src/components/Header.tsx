7a6a577d88043eb05885dfbf501d4064375ce6ea
Rorisang Putu <rorisangputu@gmail.com>
2024-12-18 00:48:40 +0200

    changing header based on user logged in or not

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
 
 const Header = () => {
+  const { isLoggedIn } = useAppContext();
   return (
     <div className="bg-blue-800 py-6">
       <div className="w-[90%] md:w-[60%] mx-auto flex justify-between items-center">
@@ -8,13 +10,39 @@ const Header = () => {
           <Link to={"/"}>MernHolidays.com</Link>
         </span>
         <span className="flex space-x-2">
-          <Link
-            to={"/sign-in"}
-            className="p-3 bg-white text-blue-600 items-center 
-            flex font-bold hover:bg-gray-100"
-          >
-            Sign In
-          </Link>
+          {isLoggedIn ? (
+            <div className="flex gap-5 items-center">
+              <Link
+                className="text-white font-semibold cursor-pointer"
+                to={"/my-bookings"}
+              >
+                My bookings
+              </Link>
+              <Link
+                className="text-white font-semibold cursor-pointer"
+                to={"/my-hotels"}
+              >
+                My Hotels
+              </Link>
+              <Link
+                to={"/sign-out"}
+                className="p-3 bg-white text-blue-600 items-center 
+                  flex font-bold hover:bg-gray-100"
+              >
+                Sign Out
+              </Link>
+            </div>
+          ) : (
+            <>
+              <Link
+                to={"/sign-in"}
+                className="p-3 bg-white text-blue-600 items-center 
+                flex font-bold hover:bg-gray-100"
+              >
+                Sign In
+              </Link>
+            </>
+          )}
         </span>
       </div>
     </div>
