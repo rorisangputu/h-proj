43d3c8d7ae15ec5285d69ab44d949c4aac458873
Rorisang Putu <rorisangputu@gmail.com>
2024-12-17 22:00:45 +0200

    styling toast component and adding message

--- frontend/src/components/Toast.tsx
+++ frontend/src/components/Toast.tsx
@@ -4,7 +4,13 @@ type ToastProps = {
 };
 
 const Toast = ({ message, type }: ToastProps) => {
-  return <div>Toast</div>;
+  return (
+    <div>
+      <div className="flex justify-center items-center">
+        <span className="text-lg font-semibold">{message}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Toast;
