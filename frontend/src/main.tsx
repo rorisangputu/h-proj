6ab5ef447bc8d0362baf895fd8c6884dc32ebc28
Rorisang Putu <rorisangputu@gmail.com>
2024-12-17 08:48:22 +0200

    initialising query client in main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient } from "react-query";
+
+const queryClient = new QueryClient({
+  
+})
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
