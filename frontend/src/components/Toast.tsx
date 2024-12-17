268e46283c1e66deaa77e3a17966ebdbc10c13fa
Rorisang Putu <rorisangputu@gmail.com>
2024-12-17 22:08:20 +0200

    setting up timer to remove toast after 5 seconds

--- frontend/src/components/Toast.tsx
+++ frontend/src/components/Toast.tsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
+
 type ToastProps = {
   message: string;
   type: "SUCCESS" | "ERROR";
+  onClose: () => void;
 };
 
-const Toast = ({ message, type }: ToastProps) => {
+const Toast = ({ message, type, onClose }: ToastProps) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, 5000);
+  });
   const styles =
     type === "SUCCESS"
       ? "fixed top-4 right-4 z-50 p-4 rounded-md bg-green-600 text-white max-w-md"
