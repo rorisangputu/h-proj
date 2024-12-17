f56b0b1d365a47f25de70a9455252843f68a0f52
Rorisang Putu <rorisangputu@gmail.com>
2024-12-18 00:36:07 +0200

    creating validateToken func

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -2,7 +2,7 @@ import { RegisterFormData } from "./pages/Register";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const register = async (formData:RegisterFormData) => {
+export const register = async (formData: RegisterFormData) => {
     const response = await fetch(`${API_BASE_URL}/api/users/register`, {
         method: "POST",
         credentials: "include",
@@ -17,4 +17,16 @@ export const register = async (formData:RegisterFormData) => {
     if (!response.ok) {
         throw new Error(responseBody.message);
     }
+};
+
+export const validateToken = async () => {
+    const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
+        credentials: "include"
+    });
+
+    if (!response.ok)
+    {
+        throw new Error("Token invalid")
+    }
+    return response.json();
 }
