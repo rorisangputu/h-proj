27470053ddc0bdeda9d7b060d8155f75adac4c54
Rorisang Putu <rorisangputu@gmail.com>
2024-12-17 08:19:55 +0200

    creating local file variable for env api url

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,5 +1,7 @@
 import { RegisterFormData } from "./pages/Register";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const register = async (formData:RegisterFormData) => {
     const response = await fetch(`${API_BASE_URL}/api/users/register`, {
         method: "POST",
