58a2546216d66e5725513dfbf8c6f71fbaae9fd5
Rorisang Putu <rorisangputu@gmail.com>
2024-12-18 00:17:24 +0200

    logic to return errors if token doesnt exist, logic to verify token

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,5 +1,16 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken'
 
 export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies["auth_token"]
+    const token = req.cookies["auth_token"];
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorised" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string)
+    } catch (error) {
+        return res.status(401).json({ message: "Unauthorised" });
+    }
 }
