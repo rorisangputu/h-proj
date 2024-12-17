89ac22f087b6d1b5312ab6b6b1fb4fc57c481cf4
Rorisang Putu <rorisangputu@gmail.com>
2024-12-17 08:50:37 +0200

    wrapping app in react query provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,16 +3,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient({
-  
-})
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
