db24e2939f199843afa7decd223c48287474cf98
Rorisang Putu <rorisangputu@gmail.com>
2024-12-17 22:04:40 +0200

    styles logic for error type

--- frontend/src/components/Toast.tsx
+++ frontend/src/components/Toast.tsx
@@ -4,9 +4,10 @@ type ToastProps = {
 };
 
 const Toast = ({ message, type }: ToastProps) => {
-
-    const styles = type === "SUCCESS"
-        ? "fixed top-4 right-4 z-50 p-4 rounded-md bg-green-600 text-white max-w-md"
+  const styles =
+    type === "SUCCESS"
+      ? "fixed top-4 right-4 z-50 p-4 rounded-md bg-green-600 text-white max-w-md"
+      : "fixed top-4 right-4 z-50 p-4 rounded-md bg-red-600 text-white max-w-md";
   return (
     <div>
       <div className="flex justify-center items-center">
