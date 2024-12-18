182e252e7823e4a6c45267579618eb5c24302dbb
Rorisang Putu <rorisangputu@gmail.com>
2024-12-18 23:15:00 +0200

    styling form, addinf spacing

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -31,7 +31,7 @@ const SignIn = () => {
   return (
     <form onSubmit={onSubmit} className="flex flex-col gap-5">
       <h2 className="text-3xl font-bold">Sign In</h2>
-      <div className="">
+      <div className="flex flex-col gap-4">
         <label htmlFor="" className="text-gray-700 text-sm font-bold flex-1">
           Email
           <input
