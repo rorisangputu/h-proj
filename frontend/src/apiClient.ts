ed4a285eb1f2776519ed3ba2b67fc0fa1cf225be
Rorisang Putu <rorisangputu@gmail.com>
2024-12-18 22:44:04 +0200

    adding request method

--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -22,6 +22,7 @@ export const register = async (formData: RegisterFormData) => {
 
 export const signIn = async (formData: SignInFormData) => {
     const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+        method: "POST",
         credentials: "include",
         headers: {
             "Content-Type": "application/son"
