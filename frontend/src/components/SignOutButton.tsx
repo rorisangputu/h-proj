ba45f3bb2f3aec3f2f2c2ba1264864dd5ff9ac08
Rorisang Putu <rorisangputu@gmail.com>
2024-12-18 23:43:23 +0200

    styling button

--- frontend/src/components/SignOutButton.tsx
+++ frontend/src/components/SignOutButton.tsx
@@ -1,7 +1,12 @@
-import React from "react";
-
 const SignOutButton = () => {
-  return <div>SignOutButton</div>;
+  return (
+    <button
+      className="p-3 bg-white text-blue-600 items-center 
+        flex font-bold hover:bg-gray-100"
+    >
+      Sign Out
+    </button>
+  );
 };
 
 export default SignOutButton;
