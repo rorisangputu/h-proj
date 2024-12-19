a07033b569febec5534267a443ccefe9c088a04f
Rorisang Putu <rorisangputu@gmail.com>
2024-12-19 23:08:33 +0200

    initialising sign in test

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -1,10 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('should allow user to sign in', async ({ page }) => {
+  
 });
 
 test('get started link', async ({ page }) => {
