2f05e1556de47030a8130d042a36fab50d77d7a9
Rorisang Putu <rorisangputu@gmail.com>
2024-12-19 21:25:58 +0200

    using correct cookie name and sending back response

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -53,9 +53,11 @@ router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
 });
 
 router.post("/logout", (req: Request, res: Response) => {
-    res.cookie("auth_token", "", {
+    res.cookie("booking_auth_token", "", {
         expires: new Date(0),
     });
+
+    res.status(200).send();
 });
 
 export default router;
