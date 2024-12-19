a0fede622792fc1eab5920c60ea19e033e50742f
Rorisang Putu <rorisangputu@gmail.com>
2024-12-19 23:32:10 +0200

    removing uneeded test

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -22,12 +22,4 @@ test('should allow user to sign in', async ({ page }) => {
   await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
