247fd4704783ec4d9c5a74e38539c2b7b5f9f131
Rorisang Putu <rorisangputu@gmail.com>
2024-12-19 23:46:13 +0200

    initialising register test

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -22,4 +22,7 @@ test('should allow user to sign in', async ({ page }) => {
   await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
 });
 
+test("should allow user to register", async ({ page }) => {
+  await page.goto(UI_URL);
+})
 
