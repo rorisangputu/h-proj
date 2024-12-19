6724f51b21d4e987215f6ebd8746c8ee3e4c4ee8
Rorisang Putu <rorisangputu@gmail.com>
2024-12-20 00:23:16 +0200

    changing test creds

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -31,11 +31,11 @@ test("should allow user to register", async ({ page }) => {
   
   await expect(page.getByRole("heading", { name: "Create an account" })).toBeVisible();
 
-  await page.locator("[name=firstName]").fill("MarkusTest");
-  await page.locator("[name=lastName]").fill("JoosteTest");
-  await page.locator("[name=email]").fill("markustest@gmail.com");
-  await page.locator("[name=password]").fill("markustest@gmail.com");
-  await page.locator("[name=confirmPassword]").fill("markustest@gmail.com");
+  await page.locator("[name=firstName]").fill("paulTest");
+  await page.locator("[name=lastName]").fill("paulinhoTest");
+  await page.locator("[name=email]").fill("paultest@gmail.com");
+  await page.locator("[name=password]").fill("paultest@gmail.com");
+  await page.locator("[name=confirmPassword]").fill("paultest@gmail.com");
 
   await page.getByRole("button", { name: "Create Account" }).click();
 
