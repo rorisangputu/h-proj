db08e2c437e1a0a36b4363a2d27cde296f3b1c91
Rorisang Putu <rorisangputu@gmail.com>
2024-12-21 00:39:22 +0200

    creating types for form data

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -1,9 +1,19 @@
-
+export type HotelFormData = {
+  name: string;
+  city: string;
+  country: string;
+  description: string;
+  type: string;
+  pricePerNight: number;
+  starRating: number;
+  adultCount: number;
+  childCount: number;
+  facilities: string[];
+  imageFiles: FileList;
+};
 
 const ManageHotelForm = () => {
-  return (
-    <div>ManageHotelForm</div>
-  )
-}
+  return <div>ManageHotelForm</div>;
+};
 
-export default ManageHotelForm
+export default ManageHotelForm;
