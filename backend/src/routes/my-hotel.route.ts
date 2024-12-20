9338865707fdd800a4467f5ddab404954200dadc
Rorisang Putu <rorisangputu@gmail.com>
2024-12-20 22:59:30 +0200

    initialisng multer

--- backend/src/routes/my-hotel.route.ts
+++ backend/src/routes/my-hotel.route.ts
@@ -3,7 +3,14 @@ import multer from 'multer'
 
 const router = express.Router();
 
-const storage = multer.memoryStorage();
+const storage = multer.memoryStorage(); //want to store images in memory
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024 //5mb,
+
+    }
+})
 
 //api/my-hotel
 router.post('/', async (req: Request, res: Response) => {
