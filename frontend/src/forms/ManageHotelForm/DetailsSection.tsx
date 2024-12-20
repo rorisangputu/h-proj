93b540920a7c8ffb92ce90e444ebf5f79ed35965
Rorisang Putu <rorisangputu@gmail.com>
2024-12-21 00:56:23 +0200

    importing and using formdata type

--- frontend/src/forms/ManageHotelForm/DetailsSection.tsx
+++ frontend/src/forms/ManageHotelForm/DetailsSection.tsx
@@ -1,10 +1,11 @@
 import { useFormContext } from "react-hook-form";
+import { HotelFormData } from "./ManageHotelForm";
 
 const DetailsSection = () => {
   const {
     register,
     formState: { errors },
-  } = useFormContext();
+  } = useFormContext<HotelFormData>();
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-3xl font-bold mb-3">Add Hotel</h1>
