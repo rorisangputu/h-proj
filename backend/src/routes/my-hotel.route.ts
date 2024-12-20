76188345f64637a2f12c3c5466e9133d8eaabae3
Rorisang Putu <rorisangputu@gmail.com>
2024-12-20 22:57:01 +0200

    installing and importing multer

--- backend/src/routes/my-hotel.route.ts
+++ backend/src/routes/my-hotel.route.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
+import multer from 'multer'
 
 const router = express.Router();
+
+const storage = multer.memoryStorage();
 
 //api/my-hotel
 router.post('/', async (req: Request, res: Response) => {
