825336e2b12a2f83d5b33ca5bfc516c72e7b7006
Rorisang Putu <rorisangputu@gmail.com>
2024-12-21 22:44:54 +0200

    importing facility component to form

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -1,6 +1,7 @@
 import { FormProvider, useForm } from "react-hook-form";
 import DetailsSection from "./DetailsSection";
 import TypeSection from "./TypeSection";
+import FacilitySection from "./FacilitySection";
 
 export type HotelFormData = {
   name: string;
@@ -23,6 +24,7 @@ const ManageHotelForm = () => {
       <form className="flex flex-col gap-10">
         <DetailsSection />
         <TypeSection />
+        <FacilitySection />
       </form>
     </FormProvider>
   );
