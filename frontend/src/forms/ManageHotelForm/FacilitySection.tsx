33965a52777149413796300443e2b4ff0c6f835e
Rorisang Putu <rorisangputu@gmail.com>
2024-12-21 22:40:08 +0200

    creating facility section component

---
+++ frontend/src/forms/ManageHotelForm/FacilitySection.tsx
@@ -0,0 +1,9 @@
+import { useFormContext } from "react-hook-form";
+import { hotelTypes } from "../../config/hotel-options-config";
+import { HotelFormData } from "./ManageHotelForm";
+
+const FacilitySection = () => {
+  return <div>FacilitySection</div>;
+};
+
+export default FacilitySection;
