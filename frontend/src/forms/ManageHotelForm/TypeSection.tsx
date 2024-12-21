cccd03a046dfcbe74d9aabc6b9534142933e549d
Rorisang Putu <rorisangputu@gmail.com>
2024-12-21 21:26:58 +0200

    creating elements that will display hotel types

--- frontend/src/forms/ManageHotelForm/TypeSection.tsx
+++ frontend/src/forms/ManageHotelForm/TypeSection.tsx
@@ -6,6 +6,15 @@ const TypeSection = () => {
   return (
     <div>
       <h2 className="text-2xl font-bold mb-3">Type</h2>
+      <div className="grid grid-cols-5 gap-2">
+        {hotelTypes.map((type) => (
+          <label htmlFor="">
+                <input type="radio" value={type} {...register("type", {
+                    required: "This field is required"
+                })} />
+          </label>
+        ))}
+      </div>
     </div>
   );
 };
