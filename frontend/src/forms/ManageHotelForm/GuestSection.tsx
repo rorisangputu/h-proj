34011c2532aaac2ccff7d2b911911cde480b6315
Rorisang Putu <rorisangputu@gmail.com>
2024-12-21 23:47:44 +0200

    removing min val for child count

--- frontend/src/forms/ManageHotelForm/GuestSection.tsx
+++ frontend/src/forms/ManageHotelForm/GuestSection.tsx
@@ -29,7 +29,6 @@ const GuestSection = () => {
           <input
             type="number"
             id="childCount"
-            min={1}
             className="border rounded w-full py-2 px-2 mt-3 font-normal"
             {...register("childCount", { required: "This field is required" })}
           />
