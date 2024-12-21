563ac39c0842b62e86dea41fee34fbb823f2c9fa
Rorisang Putu <rorisangputu@gmail.com>
2024-12-21 21:37:56 +0200

    adding styles for selected type

--- frontend/src/forms/ManageHotelForm/TypeSection.tsx
+++ frontend/src/forms/ManageHotelForm/TypeSection.tsx
@@ -9,7 +9,13 @@ const TypeSection = () => {
       <h2 className="text-2xl font-bold mb-3">Type</h2>
       <div className="grid grid-cols-5 gap-2">
         {hotelTypes.map((type) => (
-          <label className={typeWatch === type ? "" : ""}>
+          <label
+            className={
+              typeWatch === type
+                ? "cursor-pointer bg-blue-300 text-sm rounded-full px-4 py-2 font-semibold"
+                : ""
+            }
+          >
             <input
               type="radio"
               value={type}
