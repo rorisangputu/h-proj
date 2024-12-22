abe5e4d9719c112ffe949fd53ce1eea72cab335e
Rorisang Putu <rorisangputu@gmail.com>
2024-12-22 21:54:47 +0200

    creating beforeEach test that will run before testing other tests in the file

---
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+const UI_URL = "http://localhost:5173/"
+
+test.beforeEach(async ({ page }) => {
+    await page.goto(UI_URL);
+    
+      //get the sign in button
+      await page.getByRole("link", { name: "Sign In" }).click();
+    
+      await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+    
+      await page.locator("[name=email]").fill("1@1.com");
+      await page.locator("[name=password]").fill("password123");
+    
+      await page.getByRole("button", { name: "Log In" }).click();
+    
+    await expect(page.getByText("Sign In Successful")).toBeVisible();
+})
