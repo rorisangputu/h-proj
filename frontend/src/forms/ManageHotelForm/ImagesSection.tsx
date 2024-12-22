b38d24de804fa1aee5818002cbe26f521c6e57d8
Rorisang Putu <rorisangputu@gmail.com>
2024-12-22 12:22:55 +0200

    binding image files to formstate and validating input

--- frontend/src/forms/ManageHotelForm/ImagesSection.tsx
+++ frontend/src/forms/ManageHotelForm/ImagesSection.tsx
@@ -11,7 +11,21 @@ const ImagesSection = () => {
     <div>
       <h2 className="text-2xl font-bold mb-3">Images</h2>
       <div className="border rounded p-4 flex flex-col gap-4">
-        <input type="file" />
+        <input
+          type="file"
+          {...register("imageFiles", {
+            validate: (imageFiles) => {
+              const totalLength = imageFiles.length;
+              if (totalLength === 0) {
+                return "At least one image should be added";
+              }
+
+              if (totalLength > 6) {
+                return "Images cannot be more than six (6)";
+              }
+            },
+          })}
+        />
       </div>
     </div>
   );
