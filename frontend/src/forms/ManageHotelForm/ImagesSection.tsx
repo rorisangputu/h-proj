759ef77e3d1212c5da5d9b45b6090a3ba4727b6a
Rorisang Putu <rorisangputu@gmail.com>
2024-12-22 12:16:34 +0200

    destructing register and form state error functions from react hook form and passing form data types

--- frontend/src/forms/ManageHotelForm/ImagesSection.tsx
+++ frontend/src/forms/ManageHotelForm/ImagesSection.tsx
@@ -1,6 +1,12 @@
-
+import { HotelFormData } from "./ManageHotelForm";
+import { useFormContext } from "react-hook-form";
 
 const ImagesSection = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<HotelFormData>();
+    
   return <div>ImagesSection</div>;
 };
 
