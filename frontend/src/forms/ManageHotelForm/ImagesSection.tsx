83b0db2185307efd52844173b48ed0d5dcf179a3
Rorisang Putu <rorisangputu@gmail.com>
2024-12-22 12:19:12 +0200

    adding input elemnt for photo upload

--- frontend/src/forms/ManageHotelForm/ImagesSection.tsx
+++ frontend/src/forms/ManageHotelForm/ImagesSection.tsx
@@ -10,6 +10,9 @@ const ImagesSection = () => {
   return (
     <div>
       <h2 className="text-2xl font-bold mb-3">Images</h2>
+      <div className="border rounded p-4 flex flex-col gap-4">
+        <input type="file" />
+      </div>
     </div>
   );
 };
