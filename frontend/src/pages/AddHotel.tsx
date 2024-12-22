4fb616456935dfaf409cac21b7fefb19d9372b84
Rorisang Putu <rorisangputu@gmail.com>
2024-12-22 14:35:09 +0200

    handling save and sending request to backend

--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,7 +1,22 @@
+import { useMutation } from "react-query";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import * as apiClient from "../apiClient";
+import { useAppContext } from "../contexts/AppContext";
 
 const AddHotel = () => {
-  return <ManageHotelForm />;
+  const { showToast } = useAppContext();
+  const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
+    onSuccess: () => {
+      showToast({ message: "Hotel create successfully", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "Something went wrong", type: "ERROR" });
+    },
+  });
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+  return <ManageHotelForm onSave={handleSave} isLoading={isLoading} />;
 };
 
 export default AddHotel;
