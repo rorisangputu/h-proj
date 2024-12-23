8a5a5cbe1187f3c9c486db8c29202deccc0a4d10
Rorisang Putu <rorisangputu@gmail.com>
2024-12-23 21:05:46 +0200

    adding existing image displaying for edit hotel

--- frontend/src/forms/ManageHotelForm/ImagesSection.tsx
+++ frontend/src/forms/ManageHotelForm/ImagesSection.tsx
@@ -5,12 +5,30 @@ const ImagesSection = () => {
   const {
     register,
     formState: { errors },
+    watch,
   } = useFormContext<HotelFormData>();
+
+  const existingImageUrls = watch("imageUrls");
 
   return (
     <div>
       <h2 className="text-2xl font-bold mb-3">Images</h2>
       <div className="border rounded p-4 flex flex-col gap-4">
+        {existingImageUrls && (
+          <div className="grid grid-cols-6 gap-4">
+            {existingImageUrls.map((url) => (
+              <div className="relative group" key={url}>
+                <img src={url} className="min-h-full object-cover" />
+                <button
+                  className="absolute inset-0 flex items-center justify-center 
+                bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 text-white"
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
         <input
           type="file"
           multiple
