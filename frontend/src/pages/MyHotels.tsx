e00f166c3cdae2337e96efd6fef6ff8ef9549693
Rorisang Putu <rorisangputu@gmail.com>
2024-12-23 12:01:06 +0200

    styling heading and button spacing

--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const MyHotels = () => {
   return (
     <div className="space-y-5">
-      <span>
+      <span className="flex justify-between items-center">
         <h1 className="text-3xl font-bold">My Hotels</h1>
         <Link
           to={"/add-hotel"}
-          className="p-3 bg-blue-700 text-white font-bold hover:bg-blue-600"
+          className="p-3 bg-blue-700 text-white font-bold text-xl hover:bg-blue-600"
         >
           Add Hotel
         </Link>
