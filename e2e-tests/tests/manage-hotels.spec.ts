fbc9ee1e1ce653dc53774b501129b1ffb3b35180
Rorisang Putu <rorisangputu@gmail.com>
2024-12-23 15:43:54 +0200

    changing issues with test

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -57,11 +57,11 @@ test("should display hotels", async ({ page }) => {
 
     await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
 
-    await expect(page.getByText("Dublin Getaways")).toBeVisible();
-    await expect(page.locator(':has-text("Lorem ipsum dolor sit amet")')).toBeVisible();
+    await expect(page.getByText("Test Hotel Bellagio")).toBeVisible();
+    await expect(page.getByText("Lorem ipsum")).toBeVisible();
     await expect(page.getByText("Dublin, Ireland")).toBeVisible();
     await expect(page.getByText("All Inclusive")).toBeVisible();
-    await expect(page.getByText("R 199 / per night")).toBeVisible();
+    await expect(page.getByText("R 119 / per night")).toBeVisible();
     await expect(page.getByText("2 adults, 3 children")).toBeVisible();
     await expect(page.getByText("2")).toBeVisible();
 
