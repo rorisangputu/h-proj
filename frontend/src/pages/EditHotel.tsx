b35b760dd7aff085fc9bd4b23c5078f0e794f3c4
Rorisang Putu <rorisangputu@gmail.com>
2024-12-23 22:44:48 +0200

    adding edit logic

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -1,10 +1,13 @@
-import { useQuery } from "react-query";
-import { useParams } from "react-router-dom";
+import { useMutation, useQuery } from "react-query";
+import { useNavigate, useParams } from "react-router-dom";
 import * as apiClient from "../apiClient";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
 
 const EditHotel = () => {
   const { hotelId } = useParams();
+  const { showToast } = useAppContext();
+  const navigate = useNavigate();
 
   const { data: hotel } = useQuery(
     "getMyHotel",
@@ -14,9 +17,27 @@ const EditHotel = () => {
     }
   );
 
+  const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
+    onSuccess: () => {
+      showToast({ message: "Updated Hotel!", type: "SUCCESS" });
+      navigate("/my-hotels");
+    },
+    onError: () => {
+      showToast({ message: "Update failed", type: "ERROR" });
+    },
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
   return (
     <div>
-      <ManageHotelForm hotel={hotel} />
+      <ManageHotelForm
+        hotel={hotel}
+        isLoading={isLoading}
+        onSave={handleSave}
+      />
     </div>
   );
 };
