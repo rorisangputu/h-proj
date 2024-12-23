ed76657c105e1034ad202a15814443aa97bbd54e
Rorisang Putu <rorisangputu@gmail.com>
2024-12-23 11:32:14 +0200

    adding page title and Add Hotel button

--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const MyHotels = () => {
-  return <div>MyHotels</div>;
+  return (
+    <div className="space-y-5">
+      <span>
+        <h1 className="text-3xl font-bold">My Hotels</h1>
+        <Link
+          to={"/add-hotel"}
+          className="p-3 bg-blue-700 text-white font-bold hover:bg-blue-600"
+        >
+          Add Hotel
+        </Link>
+      </span>
+    </div>
+  );
 };
 
 export default MyHotels;
