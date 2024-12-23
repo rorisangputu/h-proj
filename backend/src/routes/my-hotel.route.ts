5faef3bf31e1879929f0426d18e59d76d56a5840
Rorisang Putu <rorisangputu@gmail.com>
2024-12-23 22:26:32 +0200

    sending a status and returning hotel as json

--- backend/src/routes/my-hotel.route.ts
+++ backend/src/routes/my-hotel.route.ts
@@ -99,6 +99,7 @@ router.put("/:id", verifyToken, upload.array("imageFiles"), async (req: Request,
         hotel.imageUrls = [...updateImageUrls, ...(updatedHotel.imageUrls || [])];
 
         await hotel.save();
+        res.status(201).json(hotel);
     } catch (error) {
         res.status(500).json({ message: "Something went wrong" });
     }
