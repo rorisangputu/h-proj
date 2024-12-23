a9484845e34fcc08f73ffb48a9cba8f04b6db54f
Rorisang Putu <rorisangputu@gmail.com>
2024-12-23 16:00:45 +0200

    importing types from shared file

--- backend/src/models/hotel.model.ts
+++ backend/src/models/hotel.model.ts
@@ -1,21 +1,6 @@
 import { Schema, model } from "mongoose";
+import { HotelType } from "../shared/types";
 
-export type HotelType = {
-    _id: string;
-    userId: string;
-    name: string;
-    city: string;
-    country: string;
-    description: string;
-    type: string;
-    adultCount: number;
-    childCount: number;
-    facilities: string[];
-    pricePerNight: number;
-    starRating: number;
-    imageUrls: string[];
-    lastUpdated: Date;
-};
 
 const hotelSchema = new Schema<HotelType>({
     userId: { type: String, required: true },
