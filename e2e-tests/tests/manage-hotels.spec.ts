5f2ff7d4fc9397399f909097e4f4ed9be8f12695
Rorisang Putu <rorisangputu@gmail.com>
2024-12-23 15:21:15 +0200

    initialising test that aaserts the displaying of hotels

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -51,3 +51,8 @@ test("should allow user to add a hotel", async ({ page }) => {
     await page.getByRole("button", { name: "Save" }).click();
     await expect(page.getByText("Hotel created successfully")).toBeVisible();
 });
+
+test("should display hotels", async ({ page }) => {
+    await page.goto(`${UI_URL}my-hotels`);
+    
+})
