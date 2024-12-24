f6a3e39285f454031a23badecfdb7faec60ee484
Rorisang Putu <rorisangputu@gmail.com>
2024-12-24 14:03:28 +0200

    adding skip func and items fetching limit

--- backend/src/routes/hotels.route.ts
+++ backend/src/routes/hotels.route.ts
@@ -8,9 +8,10 @@ router.get("/search", async (req: Request, res: Response) => {
         const pageSize = 5;
         const pageNum = parseInt(req.query.page ? req.query.page.toString() : "1");
         
-        //If user asks for page 3 logic is to skip 2 pages
-        const skip = (pageNum - 1) * pageSize; //Telling db to skip pages
-        const hotels = await Hotel.find(); 
+        //If user asks for page 3 logic is to skip 2 pages and 10 items
+        const skip = (pageNum - 1) * pageSize; //Telling db to skip items
+
+        const hotels = await Hotel.find().skip(skip).limit(pageSize); 
     } catch (error) {
         console.log("error", error);
         res.status(500).json({ message: "Something went wrong" });
