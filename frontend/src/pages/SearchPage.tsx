46d2e8d1d302bfe8eea36bf32fd3031365855924
Rorisang Putu <rorisangputu@gmail.com>
2024-12-24 22:33:34 +0200

    commit

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { useSearchContext } from "../contexts/searchContext";
 
 const SearchPage = () => {
   const search = useSearchContext();
-    console.log(search);
-    
+  console.log(search);
+
   return <div>SearchPage</div>;
 };
 
