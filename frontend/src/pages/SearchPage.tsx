fd69d78eb98c87eaf01d3b7a6a822627c792b5ea
Rorisang Putu <rorisangputu@gmail.com>
2024-12-24 22:30:00 +0200

    importing search context and loging search results to console

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useSearchContext } from "../contexts/searchContext";
 
 const SearchPage = () => {
+  const search = useSearchContext();
+    console.log(search);
+    
   return <div>SearchPage</div>;
 };
 
