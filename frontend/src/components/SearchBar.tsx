ba1b1405c77633e758de8194a3ff2c7e47da2a2b
Rorisang Putu <rorisangputu@gmail.com>
2024-12-24 16:46:14 +0200

    adding and styling search bar elements

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,5 +1,6 @@
-import React, { FormEvent } from "react";
+import { FormEvent } from "react";
 import { useSearchContext } from "../contexts/SearchContext";
+import { MdTravelExplore } from "react-icons/md";
 
 const SearchBar = () => {
   const search = useSearchContext();
@@ -9,11 +10,33 @@ const SearchBar = () => {
   const [adultCount, setAdultCount] = useState<number>(search.adultCount);
   const [childCount, setChildCount] = useState<number>(search.childCount);
 
-    const handleSubmit = (event: FormEvent) => {
-      event.preventDefault()
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    search.saveSearchValues(
+      destination,
+      checkIn,
+      checkOut,
+      adultCount,
+      childCount
+    );
   };
 
-  return <div>SearchBar</div>;
+  return (
+    <form
+      className="-mt-8 p-3 items-center gap-4 bg-orange-500 rounded 
+        shadow-md grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5"
+    >
+      <div className="flex flex-row items-center flex-1 bg-white p-2">
+        <MdTravelExplore size={25} className="mr-2" />
+        <input
+          type="text"
+          placeholder="Where are you going?"
+          className="text-md w-full focus:outline-none"
+          value={destination}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default SearchBar;
