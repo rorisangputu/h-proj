054c9eaf825949014015790a7e52b702b9bc1cbb
Rorisang Putu <rorisangputu@gmail.com>
2024-12-24 13:59:19 +0200

    fetching hotels for search page and initialising pagination functionality

--- backend/src/routes/hotels.route.ts
+++ backend/src/routes/hotels.route.ts
@@ -1,9 +1,17 @@
 import express, { Router, Response, Request } from 'express';
+import Hotel from '../models/hotel.model';
 
 const router = Router();
 
 router.get("/search", async (req: Request, res: Response) => {
-
+    try {
+        const pageSize = 5;
+        const pageNum = parseInt(req.query.page ? req.query.page.toString() : "1")
+        const hotels = await Hotel.find(); 
+    } catch (error) {
+        console.log("error", error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
 });
 
 export default router;
