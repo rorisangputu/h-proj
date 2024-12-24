581df6c8017c2167012923c66cf6402a8a99f2c2
Rorisang Putu <rorisangputu@gmail.com>
2024-12-24 13:53:13 +0200

    adding api route to server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 import userRoutes from './routes/users.route';
 import authRoutes from './routes/auth.route';
 import myHotelRoutes from './routes/my-hotel.route';
+import hotelRoutes from './routes/hotels.route'
 import path from 'path';
 import { v2 as cloudinary } from 'cloudinary';
 
@@ -57,6 +58,7 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/my-hotels", myHotelRoutes);
+app.use("/api/hotels", hotelRoutes);
 
 app.get("*", (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
