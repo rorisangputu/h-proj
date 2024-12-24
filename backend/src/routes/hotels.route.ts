ef1134a0b7ce45f95abeb2dace1bf8b34217f3f6
Rorisang Putu <rorisangputu@gmail.com>
2024-12-24 14:01:40 +0200

    adding page skipping logic

--- backend/src/routes/hotels.route.ts
+++ backend/src/routes/hotels.route.ts
@@ -6,7 +6,10 @@ const router = Router();
 router.get("/search", async (req: Request, res: Response) => {
     try {
         const pageSize = 5;
-        const pageNum = parseInt(req.query.page ? req.query.page.toString() : "1")
+        const pageNum = parseInt(req.query.page ? req.query.page.toString() : "1");
+        
+        //If user asks for page 3 logic is to skip 2 pages
+        const skip = (pageNum - 1) * pageSize; //Telling db to skip pages
         const hotels = await Hotel.find(); 
     } catch (error) {
         console.log("error", error);
