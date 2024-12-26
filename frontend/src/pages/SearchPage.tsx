8db1a5aa71e580290178200189bcb05a0aee1734
Rorisang Putu <rorisangputu@gmail.com>
2024-12-26 19:42:28 +0200

    fetching hotels from search query

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,8 +1,13 @@
+import { useQuery } from "react-query";
 import { useSearchContext } from "../contexts/searchContext";
+import * as apiClient from '../apiClient'
 
 const SearchPage = () => {
   const search = useSearchContext();
-  console.log(search);
+
+  const { data: hotelData } = useQuery(["searchHotels", searchParms], () => {
+    apiClient.searchHotels(searchParams);
+  });
 
   return <div>SearchPage</div>;
 };
