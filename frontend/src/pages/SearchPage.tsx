30492ccc938fa5b173fbda310f4f23f374f6e9e6
Rorisang Putu <rorisangputu@gmail.com>
2024-12-26 19:46:20 +0200

    creating object of params we expect in search req

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -5,6 +5,14 @@ import * as apiClient from '../apiClient'
 const SearchPage = () => {
   const search = useSearchContext();
 
+  const searchParams = {
+    destination: search.destination,
+    checkIn: search.checkIn.toISOString(),
+    checkOut: search.checkOut.toISOString(),
+    adultCount: search.adultCount.toString(),
+    childCount: search.childCount.toString(),
+  }
+
   const { data: hotelData } = useQuery(["searchHotels", searchParms], () => {
     apiClient.searchHotels(searchParams);
   });
