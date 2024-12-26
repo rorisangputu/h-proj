200b14d96ec2dfc8d5015e31e037d0a3b3a94e3f
Rorisang Putu <rorisangputu@gmail.com>
2024-12-27 00:14:54 +0200

    adding extra search param properties

--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -120,6 +120,11 @@ export type SearchParams = {
     adultCount?: string;
     childCount?: string;
     page?: string;
+    facilities?: string[];
+    types?: string[];
+    stars?: string[];
+    maxPrice?: string;
+    sortOption?: string;
 };
 
 export const searchHotels = async (searchParams: SearchParams): Promise<HotelSearchResponse> => {
