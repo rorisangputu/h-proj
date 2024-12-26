90deaef0b2586ccb1c798b9e8a61667a62b27848
Rorisang Putu <rorisangputu@gmail.com>
2024-12-26 21:13:58 +0200

    adding comments for seearch page sections

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -22,6 +22,7 @@ const SearchPage = () => {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-4">
+      {/* FILTER BAR */}
       <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
         <div className="space-y-5">
           <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
@@ -30,6 +31,8 @@ const SearchPage = () => {
           {/* FILTERS */}
         </div>
       </div>
+      
+      {/* SEARCH RESULTS */}
     </div>
   );
 };
