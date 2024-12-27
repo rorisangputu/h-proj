283622c13c8dfc802076bb07e3cdf105fb52eb6a
Rorisang Putu <rorisangputu@gmail.com>
2024-12-27 22:50:41 +0200

    returning errors as array and a status code if errors exist

--- backend/src/routes/hotels.route.ts
+++ backend/src/routes/hotels.route.ts
@@ -10,7 +10,7 @@ router.get("/:id",
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      
+      return res.status(400).json({ errors: errors.array() });
     }
 });
 
