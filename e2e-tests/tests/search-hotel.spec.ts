8b3c236a03513f27bec84e970cee7aa5b1bb7735
Rorisang Putu <rorisangputu@gmail.com>
2024-12-27 22:21:03 +0200

    adding before each login test to run before our search tests

--- e2e-tests/tests/search-hotel.spec.ts
+++ e2e-tests/tests/search-hotel.spec.ts
@@ -2,3 +2,19 @@ import { test, expect } from "@playwright/test";
 import path from "path";
 
 const UI_URL = "http://localhost:5173/"
+
+test.beforeEach(async ({ page }) => {
+    await page.goto(UI_URL);
+    
+    //get the sign in button
+    await page.getByRole("link", { name: "Sign In" }).click();
+    
+    await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+    
+    await page.locator("[name=email]").fill("1@1.com");
+    await page.locator("[name=password]").fill("password123");
+    
+    await page.getByRole("button", { name: "Log In" }).click();
+    
+    await expect(page.getByText("Sign In Successful")).toBeVisible();
+});
