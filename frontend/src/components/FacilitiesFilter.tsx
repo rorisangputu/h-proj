56a4632df1782721cc2cf9c96b598b43897d9b46
Rorisang Putu <rorisangputu@gmail.com>
2024-12-27 19:22:17 +0200

    making changes

--- frontend/src/components/FacilitiesFilter.tsx
+++ frontend/src/components/FacilitiesFilter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { hotelTypes } from "../config/hotel-options-config";
+import { hotelFacilities, hotelTypes } from "../config/hotel-options-config";
 
 type Props = {
   selectedFacilities: string[];
@@ -9,17 +9,17 @@ type Props = {
 const FacilitiesFilter = ({ selectedFacilities, onChange }: Props) => {
   return (
     <div className="border-b border-slate-300 pb-5">
-      <h4 className="text-md font-semibold mb-2">Hotel Type</h4>
-      {hotelTypes.map((type) => (
+      <h4 className="text-md font-semibold mb-2">Facilties</h4>
+      {hotelFacilities.map((facility) => (
         <label className="flex items-center space-x-2">
           <input
             type="checkbox"
             className="rounded"
-            value={type}
-            checked={selectedFacilities.includes(type)}
+            value={facility}
+            checked={selectedFacilities.includes(facility)}
             onChange={onChange}
           />
-          <span>{type}</span>
+          <span>{facility}</span>
         </label>
       ))}
     </div>
