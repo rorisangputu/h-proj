ee1b9b659601e92c0a2299e15e5cf6fb5226a9eb
Rorisang Putu <rorisangputu@gmail.com>
2024-12-27 19:27:42 +0200

    commit

--- frontend/src/components/FacilitiesFilter.tsx
+++ frontend/src/components/FacilitiesFilter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { hotelFacilities, hotelTypes } from "../config/hotel-options-config";
+import { hotelFacilities } from "../config/hotel-options-config";
 
 type Props = {
   selectedFacilities: string[];
