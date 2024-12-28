ef7eb960ca530e78dfd6db0d02135ec736596337
Rorisang Putu <rorisangputu@gmail.com>
2024-12-28 23:12:24 +0200

    configuring date picker for check in

--- frontend/src/forms/GuestInfoForm/GuestInfo.tsx
+++ frontend/src/forms/GuestInfoForm/GuestInfo.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
 import { useForm } from "react-hook-form";
 
 type Props = {
@@ -21,13 +23,31 @@ const GuestInfo = ({ hotelId, pricePerNight }: Props) => {
     formState: { errors },
   } = useForm<GuestInfoFormData>();
 
+  const checkIn = watch("checkIn");
+  const checkOut = watch("checkOut");
+
+  const minDate = new Date();
+  const maxDate = new Date();
+  maxDate.setFullYear(maxDate.getFullYear() + 1);
+
   return (
     <div className="bg-blue-400 flex flex-col gap-4 p-4">
       <h3 className="tex-md font-bold"> {pricePerNight}</h3>
       <form>
         <div className="grid grid-cols-1 gap-4 items-center">
-                  <div>
-                      
+          <div>
+            <DatePicker
+              selected={checkIn}
+              onChange={(date) => setCheckIn(date as Date)}
+              selectsStart
+              startDate={checkIn}
+              endDate={checkOut}
+              minDate={minDate}
+              maxDate={maxDate}
+              placeholderText="Check-in Date"
+              className="min-w-full bg-white p-2 focus:outline-none"
+              wrapperClassName="min-w-full"
+            />
           </div>
         </div>
       </form>
