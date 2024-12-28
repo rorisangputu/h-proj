dfacfc77010b1e74793d4439c1fc3a96fb69cd3f
Rorisang Putu <rorisangputu@gmail.com>
2024-12-28 23:07:39 +0200

    styling booking form

--- frontend/src/forms/GuestInfoForm/GuestInfo.tsx
+++ frontend/src/forms/GuestInfoForm/GuestInfo.tsx
@@ -20,7 +20,19 @@ const GuestInfo = ({ hotelId, pricePerNight }: Props) => {
     setValue,
     formState: { errors },
   } = useForm<GuestInfoFormData>();
-  return <div></div>;
+
+  return (
+    <div className="bg-blue-400 flex flex-col gap-4 p-4">
+      <h3 className="tex-md font-bold"> {pricePerNight}</h3>
+      <form>
+        <div className="grid grid-cols-1 gap-4 items-center">
+                  <div>
+                      
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default GuestInfo;
