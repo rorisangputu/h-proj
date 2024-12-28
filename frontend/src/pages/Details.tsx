577d89f904aff88ebebdea8746af3ce09d267cbb
Rorisang Putu <rorisangputu@gmail.com>
2024-12-28 11:50:15 +0200

    commit

--- frontend/src/pages/Details.tsx
+++ frontend/src/pages/Details.tsx
@@ -4,8 +4,12 @@ import * as apiClient from "../apiClient";
 
 const Details = () => {
   const { hotelId } = useParams();
-  const { data: hotel } = useQuery("fetchHotelById", () =>
-    apiClient.fetchHotelById(hotelId as string)
+  const { data: hotel } = useQuery(
+    "fetchHotelById",
+    () => apiClient.fetchHotelById(hotelId as string),
+    {
+      enabled: !!hotelId,
+    }
   );
   return <div>Details</div>;
 };
