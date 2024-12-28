8faf851e19bf389a2564cb3547f6333d6be6227a
Rorisang Putu <rorisangputu@gmail.com>
2024-12-28 22:53:59 +0200

    creating type props for form

---
+++ frontend/src/forms/GuestInfoForm/GuestInfo.tsx
@@ -0,0 +1,15 @@
+type Props = {
+  hotelId: string;
+  pricePerNight: number;
+};
+
+const GuestInfo = ({ hotelId, pricePerNight }: Props) => {
+    
+    return (
+        <div>
+            
+      </div>
+  );
+};
+
+export default GuestInfo;
