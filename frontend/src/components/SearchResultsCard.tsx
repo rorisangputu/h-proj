2980c9cf0f4778c5fc970bb874d628c3afe12298
Rorisang Putu <rorisangputu@gmail.com>
2024-12-28 21:33:48 +0200

    adding key for map

--- frontend/src/components/SearchResultsCard.tsx
+++ frontend/src/components/SearchResultsCard.tsx
@@ -40,7 +40,10 @@ const SearchResultsCard = ({ hotel }: Props) => {
         <div className="grid grid-cols-2 items-end whitespace-nowrap">
           <div className="flex gap-1 items-center">
             {hotel.facilities.slice(0, 3).map((facility) => (
-              <span className="bg-slate-300 p-2 rounded-lg font-bold text-xs whitespace-nowrap">
+              <span
+                key={facility}
+                className="bg-slate-300 p-2 rounded-lg font-bold text-xs whitespace-nowrap"
+              >
                 {facility}
               </span>
             ))}
