ba55d42aa754067cad3b613270436ea1c6fb3e59
Rorisang Putu <rorisangputu@gmail.com>
2024-12-28 22:38:18 +0200

    Creating guest info form component

--- frontend/src/pages/Details.tsx
+++ frontend/src/pages/Details.tsx
@@ -48,9 +48,7 @@ const Details = () => {
 
       <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr]">
         <div className="whitespace-pre-line">{hotel.description}</div>
-        <div>
-          <GuestInfo />
-        </div>
+        <div className="h-fit">{/* <GuestInfo /> */}</div>
       </div>
     </div>
   );
