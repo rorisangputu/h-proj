fb7c678476784b7c2e8a316711b4b21f85796658
Rorisang Putu <rorisangputu@gmail.com>
2024-12-29 02:22:13 +0200

    getting userId from req params and search db for user, returning user information

--- backend/src/routes/users.route.ts
+++ backend/src/routes/users.route.ts
@@ -7,7 +7,18 @@ import { verifyToken } from '../middleware/auth.middleware';
 const router = express.Router();
 
 router.get("/me", verifyToken, async (req: Request, res: Response) => {
-    
+    const userId = req.userId;
+
+    try {
+        const user = await User.findById(userId).select("-password");
+        if (!user) {
+            return res.status(400).json({ message: "User not found"})
+        }
+        res.json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Something went wrong"})
+    }
 })
 
 router.post("/register", [
