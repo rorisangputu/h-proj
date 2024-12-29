1637c79ceb523278dc2b0eb99b798cd95bda8def
Rorisang Putu <rorisangputu@gmail.com>
2024-12-29 20:41:01 +0200

    creating booking form and adding types

---
+++ frontend/src/forms/BookingForm/BookingForm.tsx
@@ -0,0 +1,11 @@
+import { UserType } from "../../../../backend/src/shared/types";
+
+type Props = {
+  currentUser: UserType;
+};
+
+const BookingForm = ({ currentUser }: Props) => {
+  return <div>BookingForm</div>;
+};
+
+export default BookingForm;
