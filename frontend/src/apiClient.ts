9808aed96b34d0a0056b5dad9deb85f5719d9084
Rorisang Putu <rorisangputu@gmail.com>
2024-12-29 02:42:28 +0200

    fixing incorrent api route

--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -5,7 +5,7 @@ import { HotelSearchResponse, HotelType, UserType }  from '../../backend/src/sha
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
 
 export const fetchCurrentUser = async(): Promise<UserType>=> {
-    const response = await fetch(`${API_BASE_URL}/api/user/me`, {
+    const response = await fetch(`${API_BASE_URL}/api/users/me`, {
         credentials: "include"
     });
 
