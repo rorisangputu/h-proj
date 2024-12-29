065b38761367ae953c4cc961c70459022fac08dc
Rorisang Putu <rorisangputu@gmail.com>
2024-12-29 02:34:10 +0200

    giving fetch promise data types

--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -1,10 +1,10 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
-import { HotelSearchResponse, HotelType }  from '../../backend/src/shared/types'
+import { HotelSearchResponse, HotelType, UserType }  from '../../backend/src/shared/types'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
 
-export const fetchCurrentUser = async() => {
+export const fetchCurrentUser = async(): Promise<UserType>=> {
     const response = await fetch(`${API_BASE_URL}/api/user/me`, {
         credentials: "include"
     });
