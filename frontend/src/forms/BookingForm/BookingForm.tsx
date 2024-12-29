f0d7f965dd6256573c79603b9bf961fc02569c57
Rorisang Putu <rorisangputu@gmail.com>
2024-12-29 20:52:12 +0200

    styling form and adding first and last name input displays

--- frontend/src/forms/BookingForm/BookingForm.tsx
+++ frontend/src/forms/BookingForm/BookingForm.tsx
@@ -13,7 +13,33 @@ type BookingFormData = {
 
 const BookingForm = ({ currentUser }: Props) => {
   const { handleSubmit, register } = useForm<BookingFormData>();
-  return <div>BookingForm</div>;
+  return (
+    <form className="grid grid-cols-1 gap-5 rounded-lg border border-slate-300 p-5">
+      <span className="text-3xl font-bold">Confirm your details</span>
+      <div className="grid grid-cols-2 gap-6">
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          First Name
+          <input
+            className="mt-1 border rounded w-full py-2 px-3 text-gray-700 bg-gray-200"
+            type="text"
+            readOnly
+            disabled
+            {...register("firstName")}
+          />
+        </label>
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Last Name
+          <input
+            className="mt-1 border rounded w-full py-2 px-3 text-gray-700 bg-gray-200"
+            type="text"
+            readOnly
+            disabled
+            {...register("lastName")}
+          />
+        </label>
+      </div>
+    </form>
+  );
 };
 
 export default BookingForm;
