de9c35d41892f8d22c6f75553de4c0aa3c022fa4
Rorisang Putu <rorisangputu@gmail.com>
2024-12-29 20:44:39 +0200

    creating type for form data

--- frontend/src/forms/BookingForm/BookingForm.tsx
+++ frontend/src/forms/BookingForm/BookingForm.tsx
@@ -5,8 +5,14 @@ type Props = {
   currentUser: UserType;
 };
 
+type BookingFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+};
+
 const BookingForm = ({ currentUser }: Props) => {
-  const {handleSubmit, register} = useForm();
+  const { handleSubmit, register } = useForm<BookingFormData>();
   return <div>BookingForm</div>;
 };
 
