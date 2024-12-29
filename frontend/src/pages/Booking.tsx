f954023eb218f5a8d06b3245ba8d8a311be298f1
Rorisang Putu <rorisangputu@gmail.com>
2024-12-29 20:42:36 +0200

    importing bookng form

--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 import * as apiClient from "../apiClient";
+import BookingForm from "../forms/BookingForm/BookingForm";
 
 const Booking = () => {
   const { data: currentUser } = useQuery(
