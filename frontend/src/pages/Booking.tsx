571849ca3fc5b61990444403006a029a4fab0ab8
Rorisang Putu <rorisangputu@gmail.com>
2024-12-29 02:39:22 +0200

    commit

--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -1,11 +1,14 @@
-import { useQuery } from 'react-query';
-import * as apiClient from '../apiClient';
+import { useQuery } from "react-query";
+import * as apiClient from "../apiClient";
 
 const Booking = () => {
-    const {data: currentUser} = useQuery("fetchCurrentUser", apiClient.fetchCurrentUser)
-  return (
-    <div>Booking</div>
-  )
-}
+  const { data: currentUser } = useQuery(
+    "fetchCurrentUser",
+    apiClient.fetchCurrentUser
+  );
+    
+  console.log(currentUser?.email);
+  return <div>Booking</div>;
+};
 
-export default Booking
+export default Booking;
