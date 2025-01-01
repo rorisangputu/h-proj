633175cad7f61988a8b0ee173f27c5b7d58ec286
Rorisang Putu <rorisangputu@gmail.com>
2025-01-01 21:55:45 +0200

    creating client side booking page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import EditHotel from "./pages/EditHotel";
 import SearchPage from "./pages/SearchPage";
 import Details from "./pages/Details";
 import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -62,6 +63,14 @@ const App = () => {
               element={
                 <Layout>
                   <Booking />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-bookings"
+              element={
+                <Layout>
+                  <MyBookings />
                 </Layout>
               }
             />
